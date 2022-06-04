a74ea97200fd4641a5dd156eb7fcac6b7e75c2cc
Rafaela Cristina Ferraz <rafaelacristina@gec.inatel.br>
2022-06-04 12:23:57 -0300

    Implementing Error 404 page

---
+++ src/pages/Erro/index.js
@@ -0,0 +1,60 @@
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import backgroundImg from "../../assets/images/unsplash_m_7p45JfXQo.png";
+
+export default function Erro() {
+  const styles = {
+    screen: {
+      display: "flex",
+      flexDirection: "column",
+      justifyItems: "center",
+      alignItems: "center",
+    },
+    backgroundImage: {
+      backgroundImage: `url(${backgroundImg})`,
+      width: "100%",
+      height: "100%",
+      padding: "0px",
+    },
+    container: {
+      position: "fixed",
+      top: "30%",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      width: "40%",
+      height: "40%",
+      color: "#003dd9",
+      background: "white",
+      borderRadius: "15px",
+      boxShadow: "0 0 20px rgba(0,0,0,0.3)",
+    },
+    title: {
+      fontSize: "40px",
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "column",
+    },
+    subTitle: {
+      fontSize: "70px",
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "column",
+    },
+  };
+
+  return (
+    <Box style={styles.screen}>
+      <Avatar
+        variant="square"
+        src={backgroundImg}
+        sx={styles.backgroundImage}
+      />
+      <Box sx={styles.container}>
+        <Typography sx={styles.title}>Error Not Found</Typography>
+        <Typography sx={styles.subTitle}>404</Typography>
+      </Box>
+    </Box>
+  );
+}
